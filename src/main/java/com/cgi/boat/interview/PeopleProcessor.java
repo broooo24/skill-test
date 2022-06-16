d18840a752f52ef6e818f997ee4fdb05537eaddf
broooo24 <84236471+broooo24@users.noreply.github.com>
2022-06-16 12:41:33 +0200

    Update PeopleProcessor.java

--- src/main/java/com/cgi/boat/interview/PeopleProcessor.java
+++ src/main/java/com/cgi/boat/interview/PeopleProcessor.java
@@ -3,38 +3,20 @@ import java.util.*;
 import java.util.stream.Collectors;
 
 public class PeopleProcessor {
-    /*
-    * Example:
-            * For input: ["John Doe", "John Silver", "Peter Doe"]
-            * Expected result would be:
-            * {
-     *  "John" -> ["Doe", "Silver"]
-     *  "Peter" -> ["Doe"
-     * }
-     */
-    static Map<String, Long> lastnamesByFirstName(List<Person> people,int limitNumber){
-        Map<String, Long> result = people.stream()
-                .collect(Collectors.groupingBy(Person::getFirstName,Collectors.counting()));
-        Map<String,Long> finalMap = new LinkedHashMap<>();
-        result.entrySet()
-                .stream()
-                .sorted(Map.Entry.<String,Long>comparingByValue().reversed())
-                .limit(limitNumber)
-                .forEachOrdered(e->finalMap.put(e.getKey(),e.getValue()));
+   static Map<String, List<String>> lastnamesByFirstname(List<Person> people){
+        //TODO: implement
+        var peopleStream = people.stream()
+                .collect(groupingBy(Person::getFirstName,
+                        Collectors.mapping(Person::getLastName, Collectors.toList())));
 
-        return finalMap;
+        return peopleStream;
     }
 
-    static Map<String, Long> firstnamesByLastname(List<Person> people,int limitNumber){
-        Map<String, Long> result = people.stream()
-                .collect(Collectors.groupingBy(Person::getLastName,Collectors.counting()));
-        Map<String,Long> finalMap = new LinkedHashMap<>();
-        result.entrySet()
-                .stream()
-                .sorted(Map.Entry.<String,Long>comparingByValue().reversed())
-                .limit(limitNumber)
-                .forEachOrdered(e->finalMap.put(e.getKey(),e.getValue()));
 
-        return finalMap;
+    static Map<String, List<String>> firstnamesByLastname(List<Person> people){
+        var peopleStream = people.stream()
+                .collect(groupingBy(Person::getLastName,
+                        Collectors.mapping(Person::getLastName, Collectors.toList())));
+        return peopleStream;
     }
 }
